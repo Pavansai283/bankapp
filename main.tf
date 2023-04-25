5cb18bb738de8d3e6d457dcced5cd3571f6b9eb6
pavan sai <pavan.sai283@gmail.com>
2023-04-25 13:33:54 +0530

    updated

---
+++ main.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "test-server" {
+  ami           = "ami-02eb7a4783e7e9317" 
+  instance_type = "t2.medium" 
+  availability_zone = "ap-south-1a"
+  vpc_security_group_ids= [aws_security_group.my_sg.id]
+  key_name = "serverkey"
+  
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = file("myEC2key.pem")
+    host     = self.public_ip
+  }  
+  
+  tags = {
+    Name = "k8s-server"
+  }
+  
+  }
+   provisioner "remote-exec" {
+     inline = [
+               "sudo apt upadte -y",
+               "sudo apt install docker.io -y",
+               "sudo snap install microk8s --clasic",
+               "sudo sleep 30",
+               "sudo microk8s status",
+               "sudo microk8s kubectl create deployment medicure-deploy --image=prafullla/healthcare-app:latest",
+               "sudo microk8s kubectl expose deployment medicure-deploy --port=8084 --type=NodePort",
+               "sudo microk8s kubectl get svc",
+               "sudo echo public IP Address of the Instance",
+               "sudo curl http://checkip.amazonaws.com",
+       ]
+   } 
+}
+
+
---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.11.0"
+    }
+  }
+}
+provider "aws" {
+region     = "ap-south-1" 
+access_key = "AKIAZAWABVFBJKK2F74Z"
+secret_key = "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" 
+}
